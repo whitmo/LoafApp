5649cd43258abd94fe2d58b40bb4ddf957b1f93d
Whit Morriss <whit@openplans.org>
2009-12-30 14:36:05 -0600

    extend spot to include all the possible geolocation info on a phone

--- loafapp/model/geo.py
+++ loafapp/model/geo.py
@@ -1,37 +1,38 @@
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import MetaData
+from loafapp.model import DeclarativeBase
 from sqlalchemy import *
 import geoalchemy as geo
-from loafapp.model import DeclarativeBase
 
-
+import pdb;pdb.set_trace()
 class Spot(DeclarativeBase):
     __tablename__ = 'spots'
     id = Column(Integer, primary_key=True)
-    name = Column(Unicode, nullable=False)
-    height = Column(Integer)
+    name = Column(Unicode, nullable=True)
+    text = Column(UnicodeText, nullable=True)
+    image_path = Column(Unicode, nullable=True)
+    altitude = Column(Float)
+    accuracy = Column(Float)
+    alt_accuracy = Column(Float)
+    heading = Column(Integer)
+    speed = Column(Float)
     created = Column(DateTime, default=datetime.now())
     geom = geo.GeometryColumn(geo.Point(2))
 
-
-class Path(DeclarativeBase):
-    __tablename__ = 'paths'
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode, nullable=False)
-    width = Column(Integer)
-    created = Column(DateTime, default=datetime.now())
-    geom = geo.GeometryColumn(geo.LineString(2))
-
-
-class Area(DeclarativeBase):
-    __tablename__ = 'areas'
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode, nullable=False)
-    depth = Column(Integer)
-    created = Column(DateTime, default=datetime.now())
-    geom = geo.GeometryColumn(geo.Polygon(2))
-
 geo.GeometryDDL(Spot.__table__)
-geo.GeometryDDL(Path.__table__)
-geo.GeometryDDL(Area.__table__)
+
+## class Path(DeclarativeBase):
+##     __tablename__ = 'paths'
+##     id = Column(Integer, primary_key=True)
+##     name = Column(Unicode, nullable=False)
+##     width = Column(Integer)
+##     created = Column(DateTime, default=datetime.now())
+##     geom = geo.GeometryColumn(geo.LineString(2))
+
+
+## class Area(DeclarativeBase):
+##     __tablename__ = 'areas'
+##     id = Column(Integer, primary_key=True)
+##     name = Column(Unicode, nullable=False)
+##     depth = Column(Integer)
+##     created = Column(DateTime, default=datetime.now())
+##     geom = geo.GeometryColumn(geo.Polygon(2))
